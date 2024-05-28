5cadcc4b343e123d4e6b85d1803896e726dc3351
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-05-28 10:18:31 -0300

    Feat: Implementação do modelo.

--- LogConverter/LogConverter/Models/LogEntry.cs
+++ LogConverter/LogConverter/Models/LogEntry.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace LogConverter.Models
 {
-    internal class LogEntry
+    public class LogEntry
     {
+        public int Size { get; set; }
+        public int StatusCode { get; set; }
+        public string CacheStatus { get; set; }
+        public string HttpMethod { get; set; }
+        public string UriPath { get; set; }
+        public double ResponseTime { get; set; }
+
+        public string FormatAgora()
+        {
+            return $"\"MINHA CDN\" {HttpMethod} {StatusCode} {UriPath} {(int)ResponseTime} {Size} {CacheStatus}";
+        }
     }
 }
